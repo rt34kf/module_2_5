eaf0c5bd5ef1b2bed74fc60598730d9791c95561
rt34kf <Shchepkina.mika@yandex.ru>
2024-07-18 00:04:57 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -8,7 +8,7 @@ def get_matrix(n, m, value):
 
 result1 = get_matrix(1, 3, 4)
 result2 = get_matrix(2, 5, 3)
-result3 = get_matrix(2,2,8)
+result3 = get_matrix(2, 2, 8)
 print(result1)
 print(result2)
 print(result3)
